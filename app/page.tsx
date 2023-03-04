816811ccfb125641440b42a290d156a4177694cc
borys35 <boryskac10@gmail.com>
2023-03-04 13:43:48 +0100

    Add images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import HomeIcon from "@/assets/icons/home.svg";
+import deadliftImage from "@/assets/images/deadlift.jpg";
 import Heading from "@/components/atoms/heading";
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -7,6 +9,13 @@ export default function Home() {
     <main className={styles.box}>
       <HomeIcon />
       <Heading level={1}>H1 Heading</Heading>
+      <Image
+        src={deadliftImage}
+        alt="Deadlift"
+        width={400}
+        height={400}
+        style={{ objectFit: "cover" }}
+      />
       Home
     </main>
   );
