a04b10fd83f9ef6c5a5f4a830f4d6eaa41421155
borys35 <boryskac10@gmail.com>
2023-03-04 11:51:52 +0100

    Add icons / Set up svgr

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import HomeIcon from "@/assets/icons/home.svg";
 import Heading from "@/components/atoms/heading";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.box}>
+      <HomeIcon />
       <Heading level={1}>H1 Heading</Heading>
       Home
     </main>
