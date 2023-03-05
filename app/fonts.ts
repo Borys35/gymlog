86acc8baae1844f25ad7d368764ee6604d5cce52
borys35 <boryskac10@gmail.com>
2023-03-05 22:17:20 +0100

    Style button component

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,7 +3,7 @@ import { Inter, Noto_Sans } from "next/font/google";
 export const inter = Inter({
   subsets: ["latin"],
   display: "swap",
-  weight: ["800", "700"],
+  weight: ["700"],
 });
 
 export const notoSans = Noto_Sans({
